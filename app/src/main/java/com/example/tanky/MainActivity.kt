2c53fe23bb733f976c791cb1bb43a4baa3641206
Mimozydui <62764133+Mimozydui@users.noreply.github.com>
2020-06-05 19:28:15 +0200

    tanky aplikace

--- app/src/main/java/com/example/tanky/MainActivity.kt
+++ app/src/main/java/com/example/tanky/MainActivity.kt
@@ -9,16 +9,64 @@ import com.koushikdutta.async.http.WebSocket
 
 class MainActivity : AppCompatActivity() {
     private var socket: WebSocket? = null
+    var clicks = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val forward = findViewById<Button>(R.id.MoveForward)
         forward.setOnClickListener{
+            clicks = clicks + 1
+            var speed = 30 * clicks
+            if(speed >= 120){
+                speed = 150
+            }
             socket?.send("""
                         {
                         "cmd":"request/move/forward",
-                        "speed":100
+                        "speed": ${speed}
+                        }
+                        """.trimIndent())
+        }
+
+        val right = findViewById<Button>(R.id.MoveRight)
+        right.setOnClickListener{
+            socket?.send("""
+                        {
+                        "cmd":"request/move/turn-right",
+                        "speed":50
+                        }
+                    """.trimIndent())
+        }
+
+        val left = findViewById<Button>(R.id.MoveLeft)
+        left.setOnClickListener{
+            socket?.send("""
+                        {
+                        "cmd":"request/move/turn-left",
+                        "speed":50
+                        }
+                    """.trimIndent())
+        }
+
+        val back = findViewById<Button>(R.id.MoveBackwards)
+        back.setOnClickListener{
+            socket?.send("""
+                        {
+                        "cmd":"request/move/backward",
+                        "speed":50
+                        }
+                    """.trimIndent())
+        }
+
+        val stop = findViewById<Button>(R.id.MoveStop)
+        stop.setOnClickListener{
+            clicks = 0
+            socket?.send("""
+                        {
+                        "cmd":"request/move/stop",
+                        "speed":0
                         }
                     """.trimIndent())
         }
@@ -26,6 +74,9 @@ class MainActivity : AppCompatActivity() {
         AsyncHttpClient.getDefaultInstance()
             .websocket("ws://tank-game-server.herokuapp.com", "ws") {ex, webSocket ->
                 socket = webSocket
+                if(ex != null){
+                    Log.i("error", ex.toString())
+                }
                 webSocket.stringCallback = WebSocket.StringCallback { s: String? ->
                     Log.i("ws", s)
                 }
